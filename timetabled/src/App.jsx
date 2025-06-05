d5dc9cd6f337c527b6c432a08ed8208ffd06d2dd
Minh Hoang Ngo <mngo12332@gmail.com>
2025-06-04 22:49:06 -0500

    upload files via git

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import './main.jsx'
+import './Calendar.jsx'
+import Calendar from './Calendar.jsx'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Itinerary for 7 days in Chicago 🏙️</h1>
+      <h2>One week in Chicago is wonderful. If want to get to know the city and see all the sights, then spending 7 days in Chicago is perfect!</h2>
+      
+      <Calendar />
+    </div>
+  )
+}
+export default App
