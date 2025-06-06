71369c117f8704158ab8894f358adf691b2555ff
Minh Hoang Ngo <mngo12332@gmail.com>
2025-06-05 23:05:36 -0500

    fix GitHub Pages deployment config

--- timetabled/vite.config.js
+++ timetabled/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: './CodePathWEB102-playground/timetabled/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
   // server: {
   //   open:true,
   //   port: 5173
