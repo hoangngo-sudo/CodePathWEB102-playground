1973a43979d7bfa636ec92d8f0e55b471ea61452
Minh Hoang Ngo <mngo12332@gmail.com>
2025-06-05 21:12:42 -0500

    update files

--- timetabled/vite.config.js
+++ timetabled/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CodePathWEB102-playground/timetabled/',
+  base: './CodePathWEB102-playground/timetabled/',
   plugins: [react()]
   // server: {
   //   open:true,
